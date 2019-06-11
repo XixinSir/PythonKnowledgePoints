f9f5a93fd992b4c6c29fc415acbbe3a2047dcd43
XixinSir <51656507+XixinSir@users.noreply.github.com>
2019-06-11 18:26:26 +0800

    Add files via upload
    操作Excel

---
+++ Excel的操作/读excel的数据.py
@@ -0,0 +1,38 @@
+import xlrd
+import sys,xdrlib
+
+def open_excel(file = "BURN IN  Condition.xlsx"):
+    try:
+        data = xlrd.open_workbook(file)
+        return data
+    except Exception as e:
+        print(str(e))
+
+'''
+根据索引获取Excel表格中的数据  参数:file:Excel文件路径
+colnameindex: 表头列明所在行的索引  by_index:表的索引
+'''
+def excel_table_byindex(file= "BURN IN  Condition.xlsx",colnameindex= 0, by_index= 0):
+    data = open_excel(file)
+    table = data.sheets()[by_index]
+    nrows = table.nrows  # 行数
+    ncols = table.ncols  # 列数
+    colnames = table.row_values(colnameindex) # 某一行的数据
+    list = []
+    for rownum in range(1,nrows):
+        row = table.row_values(rownum)
+        if row:
+            app = {}
+            for i in range(len(colnames)):
+                app[colnames[i]] = row[i]
+            list.append(app)
+    return list
+"""
+根据名称获取Excel表格中的数据  参数: 
+file:Excel文件路径   
+colnameindex: 表头列明所在行的索引
+by_name: Sheet1名称
+"""
+def excel_table_byname(file= "BURN IN  Condition.xlsx",colnameindex=0,by_name=u"Sheet1"):
+    data = open_excel(file)
+    pass
