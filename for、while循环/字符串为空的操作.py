8cb0a7dccd7369939e646f33b5e7f1c0ac6efb30
XixinSir <51656507+XixinSir@users.noreply.github.com>
2019-06-13 17:37:55 +0800

    Add files via upload

---
+++ for、while循环/字符串为空的操作.py
@@ -0,0 +1,12 @@
+if "":
+    print('if "" 返回True')
+
+if "a":
+    print('if "a" 返回True')
+
+a1 = ""
+a2 = "1"
+if not len(""):
+    print('if not len("")返回True')
+if len("1"):
+    print('if len("1")返回True')
